dddc79dba3b0c88878eaa4aa89170969c9cfee19
Jordi Badia Santaulària <jordib@teracat.com>
2024-11-04 07:58:08 +0100

    Added ResultResponseFilter to set the HttpStatusCode in the APIs based on the Error

--- Common.Api/Program.cs
+++ Common.Api/Program.cs
@@ -1,4 +1,5 @@
 using System.Globalization;
+using Deve.Common.Api;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.OpenApi.Models;
 
@@ -28,7 +29,12 @@ namespace Deve.Api
                 config.DefaultRequestCulture = new RequestCulture(Constants.DefaultLangCode);
             });
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers(options =>
+            {
+                //Comment next line if you don't want to alter the Status Code
+                options.Filters.Add<ResultResponseFilter>();
+            });
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen(options =>
