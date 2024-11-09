3166125b49e4ee9bf136d97a8d9eeef991a616f4
Jordi Badia Santaulària <jordib@teracat.com>
2024-11-09 08:55:41 +0100

    Added the AspNetCore.RateLimiting to Common.Api

---
+++ Common.Api/Helpers/TooManyRequestsMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using Deve.Api;
+
+namespace Deve.Common.Api
+{
+    /// <summary>
+    /// If the response StatusCode is TooManyRequests, return the body with the error information to the client.
+    /// </summary>
+    public class TooManyRequestsMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public TooManyRequestsMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            await _next(context);
+
+            if (context.Response.StatusCode == StatusCodes.Status429TooManyRequests)
+            {
+                string langCode = UtilsApi.GetLangCodeFromRequest(context.Request) ?? Constants.DefaultLangCode;
+
+                var response = Utils.ResultError(langCode, ResultErrorType.TooManyAttempts);
+
+                await context.Response.WriteAsJsonAsync(response);
+            }
+        }
+    }
+}
