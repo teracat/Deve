805bfa672f4b373918e98018755acfc1d38a3711
Jordi Badia Santaulària <jordib@teracat.com>
2024-12-04 09:17:41 +0100

    Maui Sample Client project created

---
+++ External/Client/SdkMaui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Deve.External.ClientApp.Maui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
