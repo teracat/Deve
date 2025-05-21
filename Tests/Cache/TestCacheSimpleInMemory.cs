c7d68c8d83d5bfbd511c7ff436701d4ceab7ae3c
Jordi Badia Santaulària <jordib@teracat.com>
2025-05-21 07:17:00 +0200

    Fixed namespace in Cache.Tests, added unit tests for RedisCache, and included additional tests in TestCacheBase.

--- Tests/Cache/TestCacheSimpleInMemory.cs
+++ Tests/Cache/TestCacheSimpleInMemory.cs
@@ -1,6 +1,6 @@
 using Deve.Tests.Cache.Fixtures;
 
-namespace Cache
+namespace Deve.Tests.Cache
 {
     public class TestCacheSimpleInMemory : TestCacheBase, IClassFixture<FixtureSimpleInMemoryCache>
     {
