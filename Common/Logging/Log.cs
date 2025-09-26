d71312aa966249ca784a3de4d19d6f4d73f3966c
Jordi Badia Santaulària <8061175+teracat@users.noreply.github.com>
2025-09-26 08:10:13 +0200

    Revert "Log improvements 1.2"

--- Common/Logging/Log.cs
+++ Common/Logging/Log.cs
@@ -1,4 +1,7 @@
-﻿namespace Deve.Logging
+﻿using System.Linq;
+using System.Text;
+
+namespace Deve.Logging
 {
     /// <summary>
     /// Helper class to write some text to the log.
@@ -23,17 +26,6 @@
         }
 
         /// <summary>
-        /// Write a formatted debug text to the log.
-        /// </summary>
-        /// <param name="format">The text to be written to the log with zero or more format items,
-        /// which correspond to objects in the args array</param>
-        /// <param name="args">An object array that contains zero or more objects to format.</param>
-        public static void Debug(string format, params object[] args)
-        {
-            Providers.List.ForEach(p => p.Debug(format, args));
-        }
-
-        /// <summary>
         /// Write some error text to all the log providers registered.
         /// </summary>
         /// <param name="text">The text to be written to the log.</param>
@@ -49,28 +41,15 @@
         /// <param name="exception">The exception to be written to the log.</param>
         public static void Error(Exception exception)
         {
-            Providers.List.ForEach(p => p.Error(exception));
-        }
+            var text = new StringBuilder(exception.Message);
+            Exception? innerEx = exception.InnerException;
+            while (innerEx is not null)
+            {
+                text.Append(" | ").Append(innerEx.Message);
+                innerEx = innerEx.InnerException;
+            }
+            Error(text.ToString());
 
-        /// <summary>
-        /// Write an Exception to the log.
-        /// </summary>
-        /// <param name="exception">The exception to be written to the log.</param>
-        /// <param name="message">The message to be written to the log with the exception.</param>
-        public static void Error(Exception exception, string message)
-        {
-            Providers.List.ForEach(p => p.Error(exception, message));
-        }
-
-        /// <summary>
-        /// Write a formatted error text to the log.
-        /// </summary>
-        /// <param name="format">The text to be written to the log with zero or more format items, 
-        /// which correspond to objects in the args array</param>
-        /// <param name="args">An object array that contains zero or more objects to format.</param>
-        public static void Error(string format, params object[] args)
-        {
-            Providers.List.ForEach(p => p.Error(format, args));
         }
         #endregion
     }
