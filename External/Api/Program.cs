f6519fc5c19ed0e316949eaba4012501ca226d05
Jordi Badia Santaulària <jordib@teracat.com>
2025-12-07 08:40:17 +0100

    Refactor ApiBuilder to use separate methods

--- External/Api/Program.cs
+++ External/Api/Program.cs
@@ -2,7 +2,7 @@ namespace Deve.External.Api
 {
     public partial class Program
     {
-        public static void Main(string[] args) => new Deve.Api.ApiBuilder().CreateAndRunApi(args);
+        public static void Main(string[] args) => Deve.Api.ApiBuilder.CreateAndRunApi(args);
 
         protected Program() { }
     }
