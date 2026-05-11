35ac1eb33be42749170aa0c3f3036d7978d3f980
Jordi Badia Santaulària <jordib@teracat.com>
2026-05-11 07:50:47 +0200

    Add workflow job to validate template commands and improve the Template Test

--- Tests/Template/TemplateTest.cs
+++ Tests/Template/TemplateTest.cs
@@ -15,9 +15,9 @@ public class TemplateTest : IClassFixture<TemplateTestFixture>
         _fixture = fixture;
     }
 
-    private static int Run(string file, string args, string workingDir)
+    private static (int ExitCode, string StdOut, string StdErr) Run(string file, string args, string workingDir)
     {
-        var p = new Process
+        using var p = new Process
         {
             StartInfo = new ProcessStartInfo
             {
@@ -38,131 +38,170 @@ public class TemplateTest : IClassFixture<TemplateTestFixture>
 
         p.WaitForExit();
 
-        Console.WriteLine(stdout);
-        Console.WriteLine(stderr);
-
-        return p.ExitCode;
+        return (p.ExitCode, stdout, stderr);
     }
 
     [Fact]
     public void Step01_UninstallTemplate()
     {
-        var exit = Run(DotNetCommand, $"new uninstall \"{_fixture.TemplateRoot}\"", _fixture.TempDir);
-        Assert.Equal(0, exit);
+        var (exitCode, _, _) = Run(DotNetCommand, $"new uninstall \"{_fixture.TemplateRoot}\"", _fixture.TempDir);
+
+        Assert.NotEqual(-1, exitCode); // This can fail if the template is not installed, but we want to proceed with the test anyway
     }
 
     [Fact]
     public void Step02_InstallTemplate()
     {
-        var exit = Run(DotNetCommand, $"new install \"{_fixture.TemplateRoot}\"", _fixture.TempDir);
-        Assert.Equal(0, exit);
+        var (exitCode, stdOut, stdErr) = Run(DotNetCommand, $"new install \"{_fixture.TemplateRoot}\"", _fixture.TempDir);
+
+        Assert.True(
+            exitCode == 0,
+            $"ExitCode: {exitCode}\n\nSTDOUT:\n{stdOut}\n\nSTDERR:\n{stdErr}");
     }
 
     [Fact]
     public void Step03_CreateProject()
     {
-        var exit = Run(DotNetCommand, $"new deve -n {TestProjectName}", _fixture.TempDir);
-        Assert.Equal(0, exit);
+        var (exitCode, stdOut, stdErr) = Run(DotNetCommand, $"new deve -n {TestProjectName}", _fixture.TempDir);
+
+        Assert.True(
+            exitCode == 0,
+            $"ExitCode: {exitCode}\n\nSTDOUT:\n{stdOut}\n\nSTDERR:\n{stdErr}");
     }
 
     [Fact]
     public void Step04_CreateModule()
     {
         var projDir = Path.Combine(_fixture.TempDir, TestProjectName);
-        var exit = Run(DotNetCommand,
+
+        var (exitCode, stdOut, stdErr) = Run(DotNetCommand,
             $"new deve-module -n Sales -P {TestProjectName} --allow-scripts yes",
             projDir);
 
-        Assert.Equal(0, exit);
+        Assert.True(
+            exitCode == 0,
+            $"ExitCode: {exitCode}\n\nSTDOUT:\n{stdOut}\n\nSTDERR:\n{stdErr}");
     }
 
     [Fact]
     public void Step05_CreateFeatureCrud()
     {
         var projDir = Path.Combine(_fixture.TempDir, TestProjectName);
-        var exit = Run(DotNetCommand,
+
+        var (exitCode, stdOut, stdErr) = Run(DotNetCommand,
             $"new deve-feature-crud -n Orders -S Order -M Sales -P {TestProjectName} --allow-scripts yes",
             projDir);
 
-        Assert.Equal(0, exit);
+        Assert.True(
+            exitCode == 0,
+            $"ExitCode: {exitCode}\n\nSTDOUT:\n{stdOut}\n\nSTDERR:\n{stdErr}");
     }
 
     [Fact]
     public void Step06_AddQueryList()
     {
         var projDir = Path.Combine(_fixture.TempDir, TestProjectName);
-        var exit = Run(DotNetCommand,
+
+        var (exitCode, stdOut, stdErr) = Run(DotNetCommand,
             $"new deve-method-query-list -n GetPending -PL Orders -S Order -M Sales -P {TestProjectName} --allow-scripts yes",
             projDir);
 
-        Assert.Equal(0, exit);
+        Assert.True(
+            exitCode == 0,
+            $"ExitCode: {exitCode}\n\nSTDOUT:\n{stdOut}\n\nSTDERR:\n{stdErr}");
     }
 
     [Fact]
     public void Step07_AddQuerySingle()
     {
         var projDir = Path.Combine(_fixture.TempDir, TestProjectName);
-        var exit = Run(DotNetCommand,
+
+        var (exitCode, stdOut, stdErr) = Run(DotNetCommand,
             $"new deve-method-query -n GetLast -PL Orders -S Order -M Sales -P {TestProjectName} --allow-scripts yes",
             projDir);
 
-        Assert.Equal(0, exit);
+        Assert.True(
+            exitCode == 0,
+            $"ExitCode: {exitCode}\n\nSTDOUT:\n{stdOut}\n\nSTDERR:\n{stdErr}");
     }
 
     [Fact]
     public void Step08_AddCommand()
     {
         var projDir = Path.Combine(_fixture.TempDir, TestProjectName);
-        var exit = Run(DotNetCommand,
+
+        var (exitCode, stdOut, stdErr) = Run(DotNetCommand,
             $"new deve-method-command -n UpdateStatus -PL Orders -S Order -M Sales -P {TestProjectName} --allow-scripts yes",
             projDir);
 
-        Assert.Equal(0, exit);
+        Assert.True(
+            exitCode == 0,
+            $"ExitCode: {exitCode}\n\nSTDOUT:\n{stdOut}\n\nSTDERR:\n{stdErr}");
     }
 
     [Fact]
     public void Step09_AddEmptyFeature()
     {
         var projDir = Path.Combine(_fixture.TempDir, TestProjectName);
-        var exit = Run(DotNetCommand,
+
+        var (exitCode, stdOut, stdErr) = Run(DotNetCommand,
             $"new deve-feature-empty -n Deliveries -S Delivery -M Sales -P {TestProjectName} --allow-scripts yes",
             projDir);
 
-        Assert.Equal(0, exit);
+        Assert.True(
+            exitCode == 0,
+            $"ExitCode: {exitCode}\n\nSTDOUT:\n{stdOut}\n\nSTDERR:\n{stdErr}");
     }
 
     [Fact]
     public void Step10_AddMethodToEmptyFeature()
     {
         var projDir = Path.Combine(_fixture.TempDir, TestProjectName);
-        var exit = Run(DotNetCommand,
+
+        var (exitCode, stdOut, stdErr) = Run(DotNetCommand,
             $"new deve-method-command -n SetDelivered -PL Deliveries -S Delivery -M Sales -P {TestProjectName} --allow-scripts yes",
             projDir);
 
-        Assert.Equal(0, exit);
+        Assert.True(
+            exitCode == 0,
+            $"ExitCode: {exitCode}\n\nSTDOUT:\n{stdOut}\n\nSTDERR:\n{stdErr}");
     }
 
     [Fact]
     public void Step11_RestoreSolution()
     {
         var projDir = Path.Combine(_fixture.TempDir, TestProjectName);
-        var exit = Run(DotNetCommand, $"restore {TestProjectName}.All.slnx", projDir);
-        Assert.Equal(0, exit);
+
+        var (exitCode, stdOut, stdErr) = Run(DotNetCommand, $"restore --verbosity normal {TestProjectName}.All.slnx", projDir);
+
+        Assert.True(
+            exitCode == 0,
+            $"ExitCode: {exitCode}\n\nSTDOUT:\n{stdOut}\n\nSTDERR:\n{stdErr}");
     }
 
     [Fact]
     public void Step12_BuildSolution()
     {
         var projDir = Path.Combine(_fixture.TempDir, TestProjectName);
-        var exit = Run(DotNetCommand, $"build --no-restore {TestProjectName}.All.slnx", projDir);
-        Assert.Equal(0, exit);
+
+        var (exitCode, stdOut, stdErr) = Run(DotNetCommand, $"build --no-restore --verbosity normal {TestProjectName}.All.slnx", projDir);
+
+        Assert.True(
+            exitCode == 0,
+            $"ExitCode: {exitCode}\n\nSTDOUT:\n{stdOut}\n\nSTDERR:\n{stdErr}");
+
     }
 
     [Fact]
     public void Step13_TestSolution()
     {
         var projDir = Path.Combine(_fixture.TempDir, TestProjectName);
-        var exit = Run(DotNetCommand, $"test --no-build --verbosity normal {TestProjectName}.All.slnx", projDir);
-        Assert.Equal(0, exit);
+
+        var (exitCode, stdOut, stdErr) = Run(DotNetCommand, $"test --no-build --verbosity normal {TestProjectName}.All.slnx", projDir);
+
+        Assert.True(
+            exitCode == 0,
+            $"ExitCode: {exitCode}\n\nSTDOUT:\n{stdOut}\n\nSTDERR:\n{stdErr}");
+
     }
 }
